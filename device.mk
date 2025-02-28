37cb0ce6a176bcc7969b427ef9daa6eca2ad7cfe
Subhashis <riveeks.smooth@gmail.com>
2025-02-28 09:11:29 +0530

    RMX1821: Build android.hardware.wifi.supplicant@1.3 and android.hardware.wifi.supplicant@1.4

--- device.mk
+++ device.mk
@@ -364,6 +364,8 @@ PRODUCT_PACKAGES += \
 	android.hardware.wifi.supplicant@1.0.vendor \
 	android.hardware.wifi.supplicant@1.1.vendor \
 	android.hardware.wifi.supplicant@1.2.vendor \
+	android.hardware.wifi.supplicant@1.3.vendor \
+	android.hardware.wifi.supplicant@1.4.vendor \
 	android.hardware.wifi.hostapd@1.0.vendor \
 	android.hardware.wifi.hostapd@1.1.vendor
 
