2595f1e01cf1644b1a5797651cbeace7b24b0cd6
Abhayabhi4721 <abhayabhi4721@gmail.com>
2025-06-06 22:16:46 +0530

    hello arrow extended

--- arrow_RMX1821.mk
+++ arrow_RMX1821.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
 
 # Inherit from RMX1821 device makefile
 $(call inherit-product, device/realme/RMX1821/device.mk)
@@ -22,8 +22,11 @@ TARGET_SCREEN_HEIGHT := 1520
 TARGET_SCREEN_WIDTH := 720
 TARGET_BOOT_ANIMATION_RES := 720
 
+# GMS build flags
+ARROW_GAPPS := true
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX1821
+PRODUCT_NAME := arrow_RMX1821
 PRODUCT_DEVICE := RMX1821
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := RMX1821
